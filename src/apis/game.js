d34fc0fdfa711ec312e5d82c79d40d01ecf446a2
FlopiVG <flopivillagarcia@gmail.com>
2018-04-17 15:30:10 +0200

    Add userBuilds mock api

---
+++ src/apis/game.js
@@ -0,0 +1,9 @@
+import { builds } from "./gameData";
+
+export function userBuilds() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(builds)
+        }, 3000)
+    })
+}
