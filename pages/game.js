f677c7aa9ac170b47fa6f7fde94cc96c8807d7d5
FlopiVG <flopivillagarcia@gmail.com>
2018-04-17 13:55:09 +0200

    Make redirect in game page when user isnt logged

--- pages/game.js
+++ pages/game.js
@@ -1,9 +1,18 @@
 import Layout from "../src/components/Layout";
 import { whoAmi } from "../src/apis";
+import Router from 'next/router'
 
 class Game extends React.Component {
-    static async getInitialProps() {
+    static async getInitialProps({ res }) {
         const userLogged = await whoAmi()
+
+        if (res && !userLogged) {
+            res.writeHead(302, { Location: '/'})
+            res.end()
+            res.finished = true
+        } else if(!userLogged) {
+            Router.replace('/')
+        }
 
         return {
           userLogged
