69d62782edd87cc81f030ff902f4f322f6f8a7fd
FlopiVG <flopivillagarcia@gmail.com>
2018-04-25 11:19:09 +0200

    Create news provide to fetch news

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,40 +1,45 @@
 import PropTypes from 'prop-types';
 import NewsItem from '../src/components/NewsItem';
-import { getNews } from '../src/apis/news';
+import NewsProvider, { Consumer } from '../src/providers/News';
 import Layout from '../src/components/Layout';
 import { whoAmi } from '../src/apis/user';
 
 class Index extends React.Component {
   static async getInitialProps() {
-    const [news, userLogged] = await Promise.all([getNews(), whoAmi()]);
+    const [userLogged] = await Promise.all([whoAmi()]);
 
     return {
-      news,
       userLogged,
     };
   }
 
   static propTypes = {
     userLogged: PropTypes.string,
-    news: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      text: PropTypes.string.isRequired,
-    }).isRequired),
   };
 
   static defaultProps = {
     userLogged: '',
-    news: [],
   };
 
   render() {
-    const { news, userLogged } = this.props;
+    const { userLogged } = this.props;
     return (
       <Layout userLogged={userLogged}>
         <div className="tile is-ancestor is-vertical">
           <h1 className="title is-1">News</h1>
-          {news.map(({ id, ...rest }) => <NewsItem key={id} {...rest} />)}
+          <NewsProvider>
+            <Consumer>
+              {({ news, loadingAll }) =>
+                (loadingAll ? (
+                  <div>Loading...</div>
+                ) : (
+                  news.map(({ _id, ...rest }) => (
+                    <NewsItem key={_id} {...rest} />
+                  ))
+                ))
+              }
+            </Consumer>
+          </NewsProvider>
         </div>
       </Layout>
     );
