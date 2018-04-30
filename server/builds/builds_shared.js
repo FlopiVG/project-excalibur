a62e2f93e83111b524dd2ddb5e77ed02cdb4f985
FlopiVG <flopivillagarcia@gmail.com>
2018-04-30 13:25:14 +0200

    Fix build shared

--- server/builds/builds_shared.js
+++ server/builds/builds_shared.js
@@ -1,12 +1,12 @@
-const { updateUserResources } = require('../resources/resources.service');
+const Resource = require('../resources/resources.model');
+const ResourcesService = require('../resources/resources.service')(Resource);
 
 const updateUserResourcesShared = data =>
   new Promise((resolve, reject) => {
-    updateUserResources(data)
+    ResourcesService.updateUserResources(data)
       .then(resolve)
       .catch(reject);
   });
-
 module.exports = {
   updateUserResourcesShared,
 };
