fcf781a6a395b2be8231f2f071dc4206618eb23e
FlopiVG <flopivillagarcia@gmail.com>
2018-05-02 16:35:06 +0200

    add users model test

---
+++ server/users/__test__/users_model.spec.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { removeMongoKeys } = require('../../utils/general');
+
+mongoose.connect(process.env.MONGO_URI_TEST);
+const User = require('../users_model');
+
+describe('users model test', () => {
+  let input;
+  beforeAll(async () => {
+    await User.remove({});
+
+    input = {
+      name: 'foo',
+      password: 'foo',
+      email: 'foo',
+    };
+  });
+
+  afterEach(async () => {
+    await User.remove({});
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  it('has a module', () => {
+    expect(User).toBeDefined();
+  });
+
+  describe('get users', () => {
+    beforeEach(async () => {
+      await User.insertMany(input, input);
+    });
+
+    it('should get users', async () => {
+      const users = await User.find({}).lean();
+
+      expect(removeMongoKeys(users)).toContainEqual(input);
+    });
+  });
+});
