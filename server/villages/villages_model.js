9927dcb914e4b267fe22d9fe3d7b40bc602a9395
FlopiVG <flopivillagarcia@gmail.com>
2018-05-09 11:12:58 +0200

    configure villages model

--- server/villages/villages_model.js
+++ server/villages/villages_model.js
@@ -3,7 +3,33 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const villageSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  idUser: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  builds: [
+    {
+      idBuild: {
+        type: Schema.Types.ObjectId,
+        ref: 'builds',
+      },
+      level: Number,
+    },
+  ],
+  resources: [
+    {
+      idResource: {
+        type: Schema.Types.ObjectId,
+        ref: 'resources',
+      },
+      quantity: Number,
+    },
+  ],
 });
 
 const villageModel = mongoose.model('villages', villageSchema);
