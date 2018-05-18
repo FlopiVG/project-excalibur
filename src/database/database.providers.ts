8c412600bf70bdfb085b50f9f71b7dedad5f1784
Flopi <flopivillagarcia@gmail.com>
2018-05-18 10:53:33 +0200

    Change mongo database url

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,8 @@ export const databaseProviders = [
   {
     provide: 'DbConnectionToken',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect('mongodb://127.0.0.1:27017/proyect-excalibur'),
+      await mongoose.connect(
+        'mongodb://testop:testop@ds155699.mlab.com:55699/proyect-excalibur',
+      ),
   },
 ];
