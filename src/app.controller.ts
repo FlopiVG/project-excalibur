df382cb502c8af05506a5d5613bc0e8ef4513a98
FlopiVG <flopivillagarcia@gmail.com>
2018-05-21 13:08:28 +0200

    Configure next

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,28 @@
-import { Get, Post, Controller, UseGuards } from '@nestjs/common';
+import * as next from 'next';
+import {
+  Get,
+  Post,
+  Controller,
+  UseGuards,
+  Response,
+  Request,
+  Next,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
+import { nextHandler } from 'main';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  root(): string {
-    return this.appService.root();
+  @Get('*')
+  root(
+    @Request() req: Request,
+    @Response() res: Response,
+    @Next() next: Function,
+  ): Function {
+    if (/api/.test(req.url)) return next();
+    return nextHandler(req, res);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import * as next from 'next';
 import { AppModule } from './app.module';
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+
+const nextApp = next({ dev });
+
+export const nextHandler = nextApp.getRequestHandler();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  await nextApp.prepare();
+  await app.listen(port);
 }
+
 bootstrap();
