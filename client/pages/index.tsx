3b807e956bda59f70592481440f2612d3cfa374c
Flopi <flopivillagarcia@gmail.com>
2018-05-22 19:20:12 +0200

    Add header logo

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 export default () => (
-  <div>
+  <div className="container">
+    <img
+      className="responsive-img"
+      src="http://elpajaroburlon.com/wp-content/uploads/2013/09/excalibur-11.jpg"
+      alt="logo"
+    />
     <h1>Index Page</h1>
     <div>
       <a className="waves-effect waves-light btn">button</a>
     </div>
+    <style scoped>
+      {`
+          img {
+            max-height: 200px;
+            width: 100%;
+          }
+        `}
+    </style>
   </div>
 );
