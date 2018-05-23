9cfd62295ddf5937e9d4f901bb096c05712d5342
FlopiVG <flopivillagarcia@gmail.com>
2018-05-23 10:12:39 +0200

    Add MONGO_URI via .env

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,11 +1,11 @@
 import * as mongoose from 'mongoose';
 
+const { MONGO_URI } = process.env
+
 export const databaseProviders = [
   {
     provide: 'DbConnectionToken',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect(
-        'mongodb://testop:testop@ds155699.mlab.com:55699/proyect-excalibur',
-      ),
+      await mongoose.connect(MONGO_URI),
   },
 ];
