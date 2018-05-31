af64af6078b37c2bd9d9cf6682ba41fca3b42045
FlopiVG <flopivillagarcia@gmail.com>
2018-05-31 14:38:18 +0200

    Get user logged permissions

--- src/authorization/authorization.controller.ts
+++ src/authorization/authorization.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Headers } from '@nestjs/common';
 import { AuthorizationService } from './authorization.service';
 import { IAuthorization } from './interfaces/IAuthorization.interface';
+import { AuthGuard } from '@nestjs/passport';
+import { IPermissions } from './interfaces/IPermissions.interface';
 
 @Controller('api/authorization')
 export class AuthorizationController {
@@ -9,5 +11,13 @@ export class AuthorizationController {
   @Get()
   findAll(): Promise<IAuthorization[]> {
     return this.authorizationService.findAll();
+  }
+
+  @Get('me')
+  @UseGuards(AuthGuard('jwt'))
+  findUserAuthorization(
+    @Headers('authorization') bearerToken: string,
+  ): Promise<IPermissions> {
+    return this.authorizationService.findUserAuthorization(bearerToken);
   }
 }
--- src/authorization/authorization.service.ts
+++ src/authorization/authorization.service.ts
@@ -1,6 +1,10 @@
+import * as jwt from 'jsonwebtoken';
 import { Model } from 'mongoose';
 import { Injectable, Inject } from '@nestjs/common';
 import { IAuthorization } from './interfaces/IAuthorization.interface';
+import { IPermissions } from './interfaces/IPermissions.interface';
+
+const { SECRET } = process.env;
 
 @Injectable()
 export class AuthorizationService {
@@ -14,6 +18,18 @@ export class AuthorizationService {
       this.authorizationModel
         .find()
         .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  findUserAuthorization(bearerToken: string): Promise<IPermissions> {
+    return new Promise((resolve, reject) => {
+      const token = bearerToken.split(' ')[1];
+      const userInfo: any = jwt.verify(token, SECRET);
+
+      this.authorizationModel
+        .findOne({ user_id: userInfo._id })
+        .then(data => resolve(data.permissions))
         .catch(reject);
     });
   }
