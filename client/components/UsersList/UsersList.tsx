c51baff0ba108c0d19cf517b1cbe587150cc07cf
FlopiVG <flopivillagarcia@gmail.com>
2018-06-05 15:17:40 +0200

    show modal when user want to delete

--- client/components/UsersList/UsersList.tsx
+++ client/components/UsersList/UsersList.tsx
@@ -4,10 +4,88 @@ import { IUsersContext } from '../../providers/Users/interfaces/IUsersContext.in
 import { IUserDto } from '../../providers/Users/dto/IUser.dto';
 import CreateUser from './CreateUser';
 
-export default class extends React.Component {
+interface IUsersListState {
+  showDeleteModal: boolean;
+  activeUserId: string;
+  actionFn(_id: string);
+}
+
+export default class extends React.Component<null, IUsersListState> {
+  constructor(props) {
+    super(props);
+
+    this.renderDeleteModal = this.renderDeleteModal.bind(this);
+  }
+
+  state = {
+    showDeleteModal: false,
+    activeUserId: '',
+    actionFn: (_id: string) => {},
+  };
+
+  handleDelete(_id, deleteFn) {}
+
+  renderDeleteModal() {
+    const { showDeleteModal, activeUserId, actionFn } = this.state;
+    return (
+      <div className={`modal ${showDeleteModal && 'is-active'}`}>
+        <div className="modal-background" />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Delete User</p>
+            <button
+              className="delete"
+              onClick={() =>
+                this.setState({
+                  showDeleteModal: false,
+                  actionFn: () => {},
+                  activeUserId: '',
+                })
+              }
+            />
+          </header>
+          <section className="modal-card-body">
+            <div className="title is-5">
+              Are you sure you want to delete this user?
+            </div>
+          </section>
+          <footer className="modal-card-foot">
+            <button
+              className="button is-danger"
+              onClick={() => {
+                actionFn(activeUserId);
+                this.setState({
+                  showDeleteModal: false,
+                  actionFn: () => {},
+                  activeUserId: '',
+                });
+              }}
+            >
+              Delete
+            </button>
+            <button
+              className="button"
+              onClick={() =>
+                this.setState({
+                  showDeleteModal: false,
+                  actionFn: () => {},
+                  activeUserId: '',
+                })
+              }
+            >
+              Cancel
+            </button>
+          </footer>
+        </div>
+      </div>
+    );
+  }
+
   render() {
+    const { showDeleteModal } = this.state;
     return (
       <UsersProvider>
+        {this.renderDeleteModal()}
         <h1 className="title">Users List</h1>
         <Consumer>
           {(context: IUsersContext) => (
@@ -40,7 +118,14 @@ export default class extends React.Component {
                         <td>
                           <button
                             className="button is-danger"
-                            onClick={() => context.deleteUser(user._id)}
+                            disabled={context.deleteUserLoading}
+                            onClick={() =>
+                              this.setState({
+                                showDeleteModal: true,
+                                activeUserId: user._id,
+                                actionFn: context.deleteUser,
+                              })
+                            }
                           >
                             <span className="icon">
                               <i className="fas fa-trash-alt" />
