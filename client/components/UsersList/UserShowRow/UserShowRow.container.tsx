246805665a23b889c37a6ae7adc53dc893158913
FlopiVG <flopivillagarcia@gmail.com>
2018-06-06 12:17:48 +0200

    Add remove user

--- client/components/UsersList/UserShowRow/UserShowRow.container.tsx
+++ client/components/UsersList/UserShowRow/UserShowRow.container.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Consumer as UserDetailsConsumer } from '../../../providers/UserDetails';
+import { Consumer as UserConsumer } from '../../../providers/Users';
 import '../../../utils/Permissions';
 
 interface IProps {
@@ -15,19 +16,25 @@ export default Cmp =>
     render() {
       const { _id, username, password, email, toggleEdit } = this.props;
       return (
-        <UserDetailsConsumer>
-          {({ permissions }) => (
-            <Cmp
-              _id={_id}
-              username={username}
-              password={`${password.slice(0, 20)} ...`}
-              email={email}
-              hasDelete={permissions.hasPermission('admin', 'read')}
-              hasEdit={permissions.hasPermission('admin', 'delete')}
-              toggleEdit={toggleEdit}
-            />
+        <UserConsumer>
+          {({ deleteUser, deleteUserLoading }) => (
+            <UserDetailsConsumer>
+              {({ permissions }) => (
+                <Cmp
+                  _id={_id}
+                  username={username}
+                  password={`${password.slice(0, 20)} ...`}
+                  email={email}
+                  hasDelete={permissions.hasPermission('admin', 'read')}
+                  hasEdit={permissions.hasPermission('admin', 'delete')}
+                  toggleEdit={toggleEdit}
+                  deleteUser={() => deleteUser(_id)}
+                  deleteUserLoading={deleteUserLoading}
+                />
+              )}
+            </UserDetailsConsumer>
           )}
-        </UserDetailsConsumer>
+        </UserConsumer>
       );
     }
   };
--- client/components/UsersList/UserShowRow/UserShowRow.tsx
+++ client/components/UsersList/UserShowRow/UserShowRow.tsx
@@ -9,9 +9,31 @@ interface IProps {
   hasDelete: boolean;
   hasEdit: boolean;
   toggleEdit: (_id: string) => void;
+  deleteUser: () => void;
+  deleteUserLoading: boolean;
 }
 
-export default class extends Component<IProps> {
+interface IState {
+  showDeleteModal: boolean;
+}
+
+export default class extends Component<IProps, IState> {
+  state = {
+    showDeleteModal: false,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.toggleDeleteModal = this.toggleDeleteModal.bind(this);
+  }
+
+  toggleDeleteModal() {
+    const { showDeleteModal } = this.state;
+
+    this.setState({ showDeleteModal: !showDeleteModal });
+  }
+
   render() {
     const {
       _id,
@@ -21,16 +43,24 @@ export default class extends Component<IProps> {
       hasDelete,
       hasEdit,
       toggleEdit,
+      deleteUser,
+      deleteUserLoading,
     } = this.props;
+    const { showDeleteModal } = this.state;
+
     return (
       <tr>
         <td>{_id}</td>
         <td>{username}</td>
         <td>{email}</td>
-        <td>{password} ...</td>
+        <td>{password}</td>
         <td>
           <If condition={hasDelete}>
-            <button className="button is-danger">
+            <button
+              className="button is-danger"
+              disabled={deleteUserLoading}
+              onClick={this.toggleDeleteModal}
+            >
               <span className="icon">
                 <i className="fas fa-trash-alt" />
               </span>
@@ -43,8 +73,36 @@ export default class extends Component<IProps> {
               </span>
             </button>
           </If>
+          <If condition={showDeleteModal}>
+            <DeleteModal cancel={this.toggleDeleteModal} confirm={deleteUser} />
+          </If>
         </td>
       </tr>
     );
   }
 }
+
+const DeleteModal = ({ cancel, confirm }) => (
+  <div className="modal is-active">
+    <div className="modal-background" />
+    <div className="modal-card">
+      <header className="modal-card-head">
+        <p className="modal-card-title">Delete User</p>
+        <button className="delete" onClick={cancel} />
+      </header>
+      <section className="modal-card-body">
+        <div className="title is-5">
+          Are you sure you want to delete this user?
+        </div>
+      </section>
+      <footer className="modal-card-foot">
+        <button className="button is-danger" onClick={confirm}>
+          Delete
+        </button>
+        <button className="button" onClick={cancel}>
+          Cancel
+        </button>
+      </footer>
+    </div>
+  </div>
+);
