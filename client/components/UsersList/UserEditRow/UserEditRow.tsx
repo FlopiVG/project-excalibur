eecf12d99f887656e2238d27fe38edb1b049ba4c
FlopiVG <flopivillagarcia@gmail.com>
2018-06-06 12:27:05 +0200

    Show edit modal

--- client/components/UsersList/UserEditRow/UserEditRow.tsx
+++ client/components/UsersList/UserEditRow/UserEditRow.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import If from '../../utils-cmp/If';
 
 interface IProps {
   _id: string;
@@ -13,6 +14,7 @@ interface IState {
   username: string;
   email: string;
   password: string;
+  showEditModal: boolean;
 }
 
 export default class extends Component<IProps, IState> {
@@ -24,11 +26,20 @@ export default class extends Component<IProps, IState> {
       username: props.username,
       email: props.email,
       password: props.password,
+      showEditModal: false,
     };
+    this.toggleEditModal = this.toggleEditModal.bind(this);
   }
+
+  toggleEditModal() {
+    const { showEditModal } = this.state;
+
+    this.setState({ showEditModal: !showEditModal });
+  }
+
   render() {
     const { _id, editCancel } = this.props;
-    const { username, email, password } = this.state;
+    const { username, email, password, showEditModal } = this.state;
 
     return (
       <tr>
@@ -43,7 +54,7 @@ export default class extends Component<IProps, IState> {
           <RenderInput field="password" value={password} type={password} />
         </td>
         <td>
-          <button className="button is-primary">
+          <button className="button is-primary" onClick={this.toggleEditModal}>
             <span className="icon">
               <i className="fas fa-check-circle" />
             </span>
@@ -53,6 +64,9 @@ export default class extends Component<IProps, IState> {
               <i className="fas fa-ban" />
             </span>
           </button>
+          <If condition={showEditModal}>
+            <EditModal cancel={this.toggleEditModal} confirm={() => {}} />
+          </If>
         </td>
       </tr>
     );
@@ -69,5 +83,30 @@ const RenderInput = ({ field, value, type = 'text' }) => (
         onChange={e => this.setState({ [field]: e.target.value })}
       />
     </p>
+  </div>
+);
+
+const EditModal = ({ cancel, confirm }) => (
+  <div className="modal is-active">
+    <div className="modal-background" />
+    <div className="modal-card">
+      <header className="modal-card-head">
+        <p className="modal-card-title">Edit User</p>
+        <button className="delete" onClick={cancel} />
+      </header>
+      <section className="modal-card-body">
+        <div className="title is-5">
+          Are you sure you want to edit this user?
+        </div>
+      </section>
+      <footer className="modal-card-foot">
+        <button className="button is-danger" onClick={confirm}>
+          Delete
+        </button>
+        <button className="button" onClick={cancel}>
+          Cancel
+        </button>
+      </footer>
+    </div>
   </div>
 );
