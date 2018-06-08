6b61187ea7adcc19e58ca8e1b8a61943b6269e70
Flopi <flopivillagarcia@gmail.com>
2018-06-08 10:05:21 +0200

    Check if user has permissions in admin section

--- client/pages/admin/users.tsx
+++ client/pages/admin/users.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import Router from 'next/router';
+import '../../utils/Permissions';
+import { UserDetailsApi } from '../../providers/UserDetails/UserDetails.api';
 import AuthProvider, { Consumer } from '../../providers/UserDetails';
 import { IUserDetailsContext } from '../../providers/UserDetails/interfaces/IUserDetailsContext.interface';
 import LayoutAdmin from '../../components/LayoutAdmin';
 import UsersList from '../../components/UsersList';
 
 export default class extends React.Component {
+  static async getInitialProps({ res }) {
+    const userDetailsApi: UserDetailsApi = new UserDetailsApi();
+    let hasPermissions: boolean = false;
+
+    try {
+      const whoami = await userDetailsApi.whoAmi();
+      hasPermissions = whoami.permissions.hasPermission('admin', 'read');
+    } catch (e) {}
+
+    if (res && !hasPermissions) {
+      res.writeHead(302, { Location: '/' });
+      res.end();
+      res.finished = true;
+    } else if (!hasPermissions) {
+      Router.replace('/');
+    } else {
+      return {};
+    }
+  }
+
   render() {
     return (
       <AuthProvider>
